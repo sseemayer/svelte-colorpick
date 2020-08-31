556e490ca464814a47c1011518a0b44546f80b28
Stefan Seemayer <stefan@seemayer.de>
2020-08-31 15:33:20 +0200

    Vendor in d3-color

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 const name = pkg.name
@@ -15,6 +16,9 @@ export default {
 	],
 	plugins: [
 		svelte(),
-		resolve()
+		resolve(),
+		commonJS({
+			include: 'node_modules/**'
+		}),
 	]
 };
